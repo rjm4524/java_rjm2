a4139755fcfb96457ad70da6030af12a17f7967b
KOREAVC <KOREAVC@DESKTOP-RDL6CAP>
2023-09-22 18:06:46 +0900

    schoolwork

---
+++ p0922_02/src/p0922_02/StuMain.java
@@ -0,0 +1,31 @@
+package p0922_02;
+
+import java.util.Scanner;
+
+public class StuMain {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		while(true) {
+		System.out.println("[학생성적 프로그램]");
+		System.out.println("------------------");
+		System.out.println("1.학생 성적입력");
+		System.out.println("2.학생 성적 출력");
+		System.out.println("3.학생 성적 수정");
+		System.out.println("0.프로그램 종료.");
+		switch(choice) {
+		case 1:
+			
+		}
+	
+		
+		
+		
+		
+		}
+		
+		
+		
+
+	}
+}
